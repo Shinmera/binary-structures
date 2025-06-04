4964b936511795dc9b631ab233c1ad4a7f3c1f2b
Yukari Hafner <shinmera@tymoon.eu>
2025-06-04 10:58:33 +0200

    Fix issues with standard accessors

--- standard-accessors.lisp
+++ standard-accessors.lisp
@@ -2,13 +2,21 @@
 
 (defmacro define-io-accessor (backend io-type)
   (let ((name (let ((*package* (find-package '#:org.shirakumo.binary-structures.types)))
-                (intern* io-type '/ backend))))
+                (intern* io-type '/ backend)))
+        (type (io-type io-type)))
     `(progn
        (export ',name '#:org.shirakumo.binary-structures.types)
-       ,(read-defun backend io-type name)
-       ,(write-defun backend io-type `(setf ,name)))))
+       ,(read-defun backend type name)
+       ,(write-defun backend type `(setf ,name)))))
 
-(defmacro define-all-io-accessors (&environment env)
+(defun try-compile (form)
+  (let* ((*standard-output* (make-broadcast-stream))
+         (*error-output* *standard-output*)
+         (*debug-io* *standard-output*))
+    (handler-case (not (nth-value 2 (compile NIL `(lambda () ,form))))
+      ((or error warning) () NIL))))
+
+(defmacro define-all-io-accessors ()
   `(progn
      ,@(loop for backend in (list-io-backends)
              nconc (loop for name in (list-io-types)
@@ -16,7 +24,10 @@
                          when (and (not (typep type 'io-structure))
                                    (not (eql (find-package "KEYWORD") (symbol-package name)))
                                    (not (member name '(T)))
-                                   (ignore-errors (macroexpand `(define-io-accessor ,backend ,name) env)))
+                                   ;; KLUDGE: this kinda sucks since we're essentially compiling
+                                   ;;         everything twice, but we have no other way of checking
+                                   ;;         whether the types are actually supported or not.
+                                   (try-compile `(define-io-accessor ,backend ,name)))
                          collect `(define-io-accessor ,backend ,name)))))
 
 (define-all-io-accessors)
